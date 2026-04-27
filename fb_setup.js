c7f45fb154014d379b5016052cf339d704c99b72
Wilkin-Porter <23351wp@hvhs.school.nz>
2026-04-28 11:41:50 +1200

    Setup

--- fb_setup.js
+++ fb_setup.js
@@ -7,20 +7,20 @@
 // Input:  n/a
 // Return: n/a
 /**************************************************************/
-  const firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    databaseURL: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId:,
-    appId:
-  };
+const firebaseConfig = {
+	apiKey: "AIzaSyCgvVl3Zn1zpd-tzw9wveN0mHVc1srkfpA",
+	authDomain: "wilkin-porter-12comp.firebaseapp.com",
+	databaseURL: "https://wilkin-porter-12comp-default-rtdb.asia-southeast1.firebasedatabase.app",
+	projectId: "wilkin-porter-12comp",
+	storageBucket: "wilkin-porter-12comp.firebasestorage.app",
+  	messagingSenderId: "303457244762",
+  	appId: "1:303457244762:web:c56d92bedc419d77046a63"
+};
 
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  // This log prints the firebase object to the console to show that it is working.
-  // As soon as you have the script working, delete this log.
-  console.log("Firebase initialize finished:");
-  console.log(firebase);
+// This log prints the firebase object to the console to show that it is working.
+// As soon as you have the script working, delete this log.
+console.log("Firebase initialize finished:");
+console.log(firebase);
