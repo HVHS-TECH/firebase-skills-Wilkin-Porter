5a3057f7ade39ecb727aa8ff3ae98054bb049220
Wilkin-Porter <23351wp@hvhs.school.nz>
2026-04-28 12:30:50 +1200

    Basic Read and Write

--- script.js
+++ script.js
@@ -18,10 +18,54 @@ const HTML_OUTPUT = document.getElementById("databaseOutput");
 // This means it replaces the whole database with message:Hello World
 /**************************************************************/
 function helloWorld(){
-  console.log("Running helloWorld()")
-  firebase.database().ref('/').set(
+	console.log("Running helloWorld()")
+	firebase.database().ref('/').set(
     {
-      message: 'Hello World!'
+		message: 'Kia Ora!'
+	}
+	)
+}
+
+
+/**************************************************************/
+// goodbyeWorld()
+// Demonstrate a minimal write to firebase
+// This function replaces the entire database with the message "Ki kite ano!"
+// 
+// This uses the set() operation to write the key:value pair "message":"Ki kite ano!"
+// The ref('/') part tells the operation to write to the base level of the database "/"
+// This means it replaces the whole database with message:Ki Kite ano!
+/**************************************************************/
+function goodbyeWorld(){
+	console.log("Running goodbyeWorld()")
+	firebase.database().ref('/').set(
+    {
+      	message: 'Ki kite ano!'
     }
-  )
+	)
+}
+
+
+/**************************************************************/
+// 
+/**************************************************************/
+function readMessage(){
+	console.log("Running readMessage()")
+	firebase.database().ref('/').child('message').once('value', displayReadMessage);
+}
+
+
+/**************************************************************/
+// 
+/**************************************************************/
+function displayReadMessage(message){
+	if (message.val() == null) {
+		console.log('An error occured when trying to read from the database.');
+		HTML_OUTPUT.style.color = 'red';
+		HTML_OUTPUT.innerHTML = 'An error occured when trying to read from the database.';
+	} else {
+		console.log('Running displayReadMessage(), the message is ' + message.val());
+		HTML_OUTPUT.style.color = 'black';
+		HTML_OUTPUT.innerHTML = message.val();
+	}
 }
